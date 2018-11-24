ef044793876be81bcc2adc9128286b4aa5234069
shi yang <shiyang888666@icloud.com>
2018-11-24 10:43:54 +0800

    add functions InitSSTable, CreatSSTable

--- 数据结构/查找/线性表的查找.c
+++ 数据结构/查找/线性表的查找.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #include <malloc.h>
 #define MAXSIZE 100
+#define OK 1
+#define ERROR 0
+
+typedef int KeyType;
+typedef char InfoType[20];
 
 typedef struct
 {
-    int key;                                       //关键字域
-    char otherinfo[20];                            //其他域
+    KeyType key;                                    //关键字域
+    InfoType otherinfo;                             //其他域
 }ElemType;
 
 typedef struct
@@ -14,8 +20,37 @@ typedef struct
     int length;
 }SSTable;
 
+/*线性表的初始化*/
+void InitSSTable(SSTable *ST)
+{
+    if(!ST) exit(0);
+    ST->R=(ElemType *)malloc(sizeof(ElemType)*MAXSIZE);
+    ST->length=0;
+}
+
+/*线性表表的创建*/
+void CreatSSTable(SSTable *ST)
+{
+    printf("请向线性表中插入元素（关键字，信息），当插入的元素的关键字为-1时，停止插入\n");
+
+    int i=1;
+    printf("请插入第%d个元素：", i);
+    KeyType key;
+    InfoType otherinfo;
+    scanf("%d，%s", &key, &otherinfo);
+    while(key!=-1)
+    {
+        ST->R[i].key=key;
+        strcpy(ST->R[i].otherinfo, otherinfo);
+        ST->length++;
+        i++;
+        printf("请插入第%d个元素：", i);
+        scanf("%d，%s", &key, &otherinfo);
+    }
+}
+
 /*顺序查找*/
-int Search(SSTable *ST, int key)
+int Search(SSTable *ST, KeyType key)
 {
     int i;
     for(i=ST->length;i>=1;--i)
@@ -27,7 +62,7 @@ int Search(SSTable *ST, int key)
 }
 
 /*设置监视哨的顺序查找*/
-int Search_Seq(SSTable *ST, int key)
+int Search_Seq(SSTable *ST, KeyType key)
 {
     ST->R[0].key=key;
     int i;
@@ -36,25 +71,26 @@ int Search_Seq(SSTable *ST, int key)
 }
 
 /*折半查找（非递归算法）*/
-int Search_Bin(SSTable *ST, int key)
+int Search_Bin(SSTable *ST, KeyType key)
 {
     int low=1;
     int high=ST->length;
-    int mid=(low+high)/2;
+    int mid;
     while(low<=high)                                //当low=high时，查找区间还有最后一个结点，还要进一步比较
     {
+        mid=(low+high)/2;
         if(key==ST->R[mid].key)
             return mid;
         else if(key<ST->R[mid].key)
-            mid=high-1;
+            high=mid-1;
         else
-            mid=low+1;
+            low=mid+1;
     }
     return 0;
 }
 
 /*折半查找（递归算法）*/
-int Search_Bin_Re(SSTable *ST, int key, int low, int high)
+int Search_Bin_Re(SSTable *ST, KeyType key, int low, int high)
 {
     if(low>high)
         return 0;
@@ -70,54 +106,37 @@ int Search_Bin_Re(SSTable *ST, int key, int low, int high)
 
 void main()
 {
-    SSTable *S;
-    S=(SSTable *)malloc(sizeof(SSTable));
-    S->R=(ElemType *)malloc(sizeof(ElemType)*MAXSIZE);
-    S->length=0;
-    printf("请输入待查询的线性表：\n");
-    int i=1;
-    int key;
-    scanf("%d",&key);
-    while(key!=-1)
-    {
-        S->R[i].key=key;
-        S->length++;
-        i++;
-        scanf("%d",&key);
-    }
-    printf("您输入的待查询的线性表为：\n");
-    for(i=1; i<=S->length;i++)
-        printf("%d ",S->R[i].key);
+    SSTable ST_1;
+    InitSSTable(&ST_1);
+    printf("创建普通线性表：\n");
+    CreatSSTable(&ST_1);
+
+    printf("线性表的遍历结果（关键字，信息）为：\n");
+    for(int i=1; i<=ST_1.length;i++)
+        printf("%d，%s  ", ST_1.R[i].key, ST_1.R[i].otherinfo);
     printf("\n");
-    printf("待查询的线性表的长度为：%d\n",S->length);
-    printf("请输入待查询的值：");
+    printf("待查询的线性表的长度为：%d\n", ST_1.length);
+
+    printf("请输入待查询的元素的关键字：");
+    KeyType key;
     scanf("%d",&key);
-    printf("顺序查找的结果为：%d\n",Search(S, key));
-    printf("设置监视哨的顺序查找的结果为：%d\n", Search_Seq(S, key));
+    printf("顺序查找的结果（线性表中的位置，关键字，信息）为：%d，%d，%s\n", Search(&ST_1, key), key, ST_1.R[Search(&ST_1, key)].otherinfo);
+    printf("设置监视哨的顺序查找的结果（线性表中的位置，关键字，信息）为：%d，%d，%s\n", Search_Seq(&ST_1, key), key, ST_1.R[Search_Seq(&ST_1, key)].otherinfo);
     printf("\n");
 
-    SSTable *S_1;
-    S_1=(SSTable *)malloc(sizeof(SSTable));
-    S_1->R=(ElemType *)malloc(sizeof(ElemType)*MAXSIZE);
-    S_1->length=0;
-    printf("请输入待查询的线性表（有序排列）：\n");
-    i=1;
-    scanf("%d",&key);
-    while(key!=-1)
-    {
-        S_1->R[i].key=key;
-        S_1->length++;
-        i++;
-        scanf("%d",&key);
-    }
-    printf("您输入的待查询的线性表为（有序排列）：\n");
-    for(i=1;i<=S_1->length;i++)
-        printf("%d ",S_1->R[i].key);
-    printf("\n");
-    printf("待查询的线性表的长度为：%d\n",S->length);
-    printf("请输入待查询的值：");
-    scanf("%d",&key);
-    printf("折半查找（非递归算法）的结果为：%d\n",Search_Bin(S_1, key));
-    printf("折半查找（递归算法）的结果为：%d\n",Search_Bin_Re(S_1, key, 1, S_1->length));
+    SSTable ST_2;
+    InitSSTable(&ST_2);
+    printf("创建有序线性表：\n");
+    CreatSSTable(&ST_2);
 
+    printf("线性表的遍历结果为：\n");
+    for(int i=1;i<=ST_2.length;i++)
+        printf("%d，%s  ", ST_2.R[i].key, ST_2.R[i].otherinfo);
+    printf("\n");
+    printf("待查询的线性表的长度为：%d\n", ST_2.length);
+
+    printf("请输入待查询的元素的关键字：");
+    scanf("%d", &key);
+    printf("折半查找（非递归算法）的结果（线性表中的位置，关键字，信息）为：%d，%d，%s\n", Search_Bin(&ST_2, key), key, ST_2.R[Search_Bin(&ST_2, key)].otherinfo);
+    printf("折半查找（递归算法）的结果（线性表中的位置，关键字，信息）为：%d，%d，%s\n", Search_Bin_Re(&ST_2, key, 1, ST_2.length), key, ST_2.R[Search_Bin_Re(&ST_2, key, 1, ST_2.length)].otherinfo);
 }
